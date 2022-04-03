b96c21c7c7f3a1c2e2a134e206ab579e8776c9a2
unknown <jzunic@pmfst.hr>
2022-04-03 12:29:28 +0200

    Dovrsene Vjezbe_4

--- Vjezbe/Vjezbe_4/calculus.py
+++ Vjezbe/Vjezbe_4/calculus.py
@@ -10,12 +10,35 @@ def derivacija(f, x, korak, metoda = "three-step"):
     
     return rez
 
-def derivacija2(f, donja_granica, gornja_granica):
+def derivacija2(f, donja_granica, gornja_granica, korak):
     lista_brojeva = np.linspace(donja_granica, gornja_granica, 100)
     lista_derivacija = []
     
     for broj in lista_brojeva:
-        lista_derivacija.append(derivacija(f, broj, 0.001))
+        lista_derivacija.append(derivacija(f, broj, korak))
     
     return lista_brojeva, lista_derivacija
-#derivacija2(lambda x: x**2, 1, 2)
+
+def integral(f, donja_granica, gornja_granica, broj_podjela):
+    dx = (gornja_granica - donja_granica)/broj_podjela
+    x_lista = np.linspace(donja_granica, gornja_granica, broj_podjela + 1)
+    donja_suma = 0
+    gornja_suma = 0
+
+    for i in range(len(x_lista) - 1):
+        donja_suma += f(x_lista[i]) * dx
+        gornja_suma += f(x_lista[i+1]) * dx
+
+    return donja_suma, gornja_suma
+
+def trapezni_integral(f, donja_granica, gornja_granica, broj_podjela):
+    dx = (gornja_granica - donja_granica)/broj_podjela
+    x_lista = np.linspace(donja_granica, gornja_granica, broj_podjela + 1)
+    integral = 0
+
+    for i in range(len(x_lista) - 1):
+        integral += dx*(f(x_lista[i]) + (f(x_lista[i+1]) - f(x_lista[i])) / 2)
+
+    return integral
+
+
--- Vjezbe/Vjezbe_4/zadatak1.py
+++ Vjezbe/Vjezbe_4/zadatak1.py
@@ -12,14 +12,51 @@ def analiticko(f, donja_granica, gornja_granica):
 
     return lista_brojeva, lista_rezultata
 
-lista_x, lista_y = calc.derivacija2(lambda x: x**3, -5, 5)
-lista_x1, lista_y1 = calc.derivacija2(lambda x: 50*np.sin(x),-5, 5)
-lista_x2, lista_y2 = analiticko(lambda x: 3*x**2, -5, 5)
-lista_x3, lista_y3 = analiticko(lambda x: 50 * np.cos(x), -5, 5)
+def slika_derivacija():
+    #Treba se zoomirat dosta na dio slike da bi se vidila odstupanja
+    # lista_x, lista_y = calc.derivacija2(lambda x: x**3, -2, 2, 0.1)
+    # lista_x1, lista_y1 = calc.derivacija2(lambda x: x**3, -2, 2, 0.01)
+    lista_x1, lista_y1 = calc.derivacija2(lambda x: np.sin(x),-2, 2, 0.1)
+    lista_x2, lista_y2 = calc.derivacija2(lambda x: np.sin(x),-2, 2, 0.01)
+    # lista_x2, lista_y2 = analiticko(lambda x: 3*x**2, -2, 2,)
+    lista_x3, lista_y3 = analiticko(lambda x: np.cos(x), -2, 2)
 
-plt.plot(lista_x, lista_y)
-plt.plot(lista_x1, lista_y1)
-plt.plot(lista_x2, lista_y2, ".")
-plt.plot(lista_x3, lista_y3, "_")
+    # plt.scatter(lista_x, lista_y, s=5)
+    plt.scatter(lista_x1, lista_y1, s=2, color="red")
+    # plt.scatter(lista_x1, lista_y1, s=5)
+    plt.scatter(lista_x2, lista_y2, s=5, color="green")
+    plt.plot(lista_x3, lista_y3)
+    plt.show()
 
+podjele = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+
+def slika_integrala():
+    donje_sume = []
+    gornje_sume = []
+
+    for podjela in podjele:
+        donja_suma, gornja_suma = calc.integral(lambda x: x**2, 1, 2, podjela)
+        donje_sume.append(donja_suma)
+        gornje_sume.append(gornja_suma)
+
+    return donje_sume, gornje_sume
+
+def slika_trapeznog_integrala():
+    integrali = []
+
+    for podjela in podjele:
+        integral = calc.trapezni_integral(lambda x:x**2, 1, 2, podjela)
+        integrali.append(integral)
+    
+    return integrali
+
+donje_sume, gornje_sume = slika_integrala()
+integrali = slika_trapeznog_integrala()
+slika_derivacija()
+
+plt.scatter(podjele, donje_sume, color="red", s=5)
+plt.scatter(podjele, gornje_sume, color="green", s=5)
+plt.scatter(podjele, integrali, color="black", s=5)
+#analiticko rjesenje za x**2 na segmentu [1,2]
+plt.plot(podjele, [7/3]*len(podjele))
 plt.show()
